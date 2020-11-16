b7b7c2efbfb2369e8b10d047813952af80b77512
mbotross <mbotross@uci.edu>
2020-11-15 20:58:58 -0800

    login and sign up screens

---
+++ app/src/main/java/com/example/justice4families/SignupActivity.kt
@@ -0,0 +1,15 @@
+package com.example.justice4families
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_signup.*
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+        back_to_login.setOnClickListener {
+            onBackPressed()
+        }
+    }
+}
