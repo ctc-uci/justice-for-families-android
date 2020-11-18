6bb9ff529a6682cc6095d6ca4704462f3450fdf7
mbotross <mbotross@uci.edu>
2020-11-18 14:24:56 -0800

    password masking

--- app/src/main/java/com/example/justice4families/SignupActivity.kt
+++ app/src/main/java/com/example/justice4families/SignupActivity.kt
@@ -2,7 +2,9 @@ package com.example.justice4families
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_signup.*
+import kotlinx.android.synthetic.main.activity_signup.view.*
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +13,19 @@ class SignupActivity : AppCompatActivity() {
         back_to_login.setOnClickListener {
             onBackPressed()
         }
+        signup_button.setOnClickListener {
+            checkPassword(password.pass_text.text.toString(), confirm_pass.confirm_text.text.toString())
+        }
     }
 
-    private fun checkPassword(password:String){
-
+    private fun checkPassword(password1:String, password2:String){
+        if(password1.isEmpty() or password2.isEmpty())
+             Toast.makeText(this, "Please Enter a Password", Toast.LENGTH_LONG).show()
+        else if(password1!=password2){
+             Toast.makeText(this, "Passwords don't match", Toast.LENGTH_LONG).show()
+        }
+        else{
+            //make sure characteristics of password is correct
+        }
     }
 }
