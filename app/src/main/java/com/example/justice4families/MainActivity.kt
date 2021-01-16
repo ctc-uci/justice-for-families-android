892dd320d4ecf7fd0ad39edd40667c4f59d9efc6
Anikait Rao <anikaitr@uci.edu>
2021-01-15 20:31:24 -0800

    refactored NumAdapter to new file

--- app/src/main/java/com/example/justice4families/MainActivity.kt
+++ app/src/main/java/com/example/justice4families/MainActivity.kt
@@ -140,29 +140,6 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    // Move to another file
-    class NumAdapter(val activity: MainActivity) : RecyclerView.Adapter<NumAdapter.NumViewHolder>() {
-
-        // Return the ViewHolder object
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumViewHolder {
-            return NumViewHolder(LayoutInflater.from(activity).inflate(R.layout.rv_child_number, parent, false))
-        }
-
-        override fun getItemCount(): Int {
-            return activity.numberList.size
-        }
-
-        override fun onBindViewHolder(holder: NumViewHolder, position: Int) {
-            holder.tvNum.text = activity.numberList[position]
-        }
-
-        class NumViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-            val tvNum = v.findViewById<TextView>(R.id.tv_number)
-        }
-
-    }
-
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.topnav_menu, menu)
