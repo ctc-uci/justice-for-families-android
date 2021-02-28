358ba627570eaf703b8dc0cf71bfd14aa2a0155e
zhajee <zoyahajee@gmail.com>
2021-02-27 17:24:06 -0800

    added EditApi

---
+++ app/src/main/java/com/example/justice4families/data/EditApi.kt
@@ -0,0 +1,31 @@
+package com.example.justice4families.data
+
+import com.example.justice4families.model.EditProfileRequest
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface EditApi {
+
+    @POST("authentication/update/picture")
+    fun editUserPicture(
+        @Body request: EditProfileRequest
+    ) : Call<ResponseBody>
+
+    @POST("authentication/update/name")
+    fun editUserName(
+        @Body request: EditProfileRequest
+    ) : Call<ResponseBody>
+
+    companion object{
+        operator fun invoke(): EditApi {
+            return Retrofit.Builder().baseUrl("https://j4f-backend.herokuapp.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(EditApi::class.java)
+        }
+    }
+}
