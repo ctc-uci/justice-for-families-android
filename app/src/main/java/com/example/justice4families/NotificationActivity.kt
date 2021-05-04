6ffd23a7f0ddadd5d18e9a50b2f87725a13511dd
Anikait Rao <anikaitr@uci.edu>
2021-05-04 16:57:12 -0700

    scrollview

--- app/src/main/java/com/example/justice4families/NotificationActivity.kt
+++ app/src/main/java/com/example/justice4families/NotificationActivity.kt
@@ -4,17 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NotificationActivity : AppCompatActivity() {
 
+    lateinit var adapter: UpdatesAdapter
+    lateinit var layoutManager : LinearLayoutManager
     private lateinit var bottomNav: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
 
-        bottomNav = findViewById(R.id.bottom_nav_notifications)
+        layoutManager = LinearLayoutManager(this)
+        var notificationRecyclerView = findViewById<RecyclerView>(R.id.recyclerViewNotifications)
+        notificationRecyclerView.isNestedScrollingEnabled = false
+        notificationRecyclerView.layoutManager = layoutManager
+
+
+        bottomNav = findViewById(R.id.bottom_nav)
         bottomNav.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.ic_addpost -> {
